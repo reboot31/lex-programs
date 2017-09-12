87527a9e0f308401c4af4f8ccf66f03063c5305a
Aayush Raj <raj.aaayush96@gmail.com>
2017-09-13 01:06:38 +0530

    first commit

---
+++ input.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	float b;
+	char c;
+	scanf("%d %f %c", &a, &b, &c);
+	printf("%d %f %c", a, b, c);
+	for (int i=0; i<a; i++)
+		do something
+			while you work
+	if this
+		then this
+	else
+		this
+	return 0;
+}
